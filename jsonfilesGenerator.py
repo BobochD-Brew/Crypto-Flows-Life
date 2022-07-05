e92f89e0c8af996a98aef77d2eb10b7527f96c1c
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-07-05 10:17:20 +0100

    Change

--- jsonfilesGenerator.py
+++ jsonfilesGenerator.py
@@ -2,8 +2,8 @@ for k in range(512):
     f = open(str(k)+".json", "w")
     f.write("""
     {
-        "name": "Crypto Flow Life #"""+str(k)+"""",
-        "description": "Crypto Life #"""+str(k)+""" \\nby @bobochdbrew",
+        "name": "Flow Life #"""+str(k)+"""",
+        "description": "Flow Life #"""+str(k)+""" \\n by @bobochdbrew",
         "animation_url": "https://bobochd-brew.github.io/Crypto-Flows-Life-Htmls/"""+str(k)+""".html",
         "image": "https://bobochd-brew.github.io/Crypto-Flows-Life-Pngs/"""+str(k)+""".png",
         "creator": "Boboch D. Brew"
