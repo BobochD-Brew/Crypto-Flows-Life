7d546393c55d2e19c47a2644354e9b09382f4017
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-07-05 13:49:26 +0100

    Rarity

--- jsonfilesGenerator.py
+++ jsonfilesGenerator.py
@@ -6,7 +6,10 @@ for k in range(512):
         "description": "Flow Life #"""+str(k)+""" \\n by @bobochdbrew",
         "animation_url": "https://bobochd-brew.github.io/Crypto-Flows-Life-Htmls/"""+str(k)+""".html",
         "image": "https://bobochd-brew.github.io/Crypto-Flows-Life-Pngs/"""+str(k)+""".png",
-        "creator": "Boboch D. Brew"
+        "creator": "Boboch D. Brew",
+        "attributes": [
+                { "trait_type": "Cells", "value": \""""+str(sum(map(lambda x: int(x),bin(k)[2:])))+"""\" }
+            ]
     }
     """)
     f.close()
