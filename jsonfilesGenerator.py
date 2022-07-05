29281033d9e6ce76fb5317d8232ebdf307458234
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-07-05 09:05:54 +0100

    Hello

---
+++ jsonfilesGenerator.py
@@ -0,0 +1,11 @@
+for k in range(512):
+    f = open(str(k)+".json", "w")
+    f.write("""
+    {
+        "name": "Crypto Flow Life #"""+str(k)+"""",
+        "description": "Crypto Life #"""+str(k)+"""" \\nby @bobochdbrew",
+        "animation_url": "https://bobochd-brew.github.io/Crypto-Flow-Life-Htmls/"""+str(k)+""".html",
+        "image": "https://bobochd-brew.github.io/Crypto-Flow-Life-Pngs/"""+str(k)+""".png",
+        "creator": "Boboch D. Brew"}
+    """)
+    f.close()
